ff776056068e0c733e96e1393450e64930167cb5
Ryan De Coster <ryandecoster@gmail.com>
2020-10-03 01:37:40 -0700

    soo soo much was done. dynamic rendering of the city, state, fixed display propertiesm trying to get the heart to conditionaly render

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,28 +1,65 @@
 import React from 'react';
 import LikeShare from './LikeShare.jsx'
+import Rating from './Rating.jsx'
+import styled from 'styled-components';
 
-function Header(props) {
+
+const Title = styled.div `
+grid-area: title;
+`
+const RatingWrapper = styled.div `
+grid-area: rating;
+
+`
+
+const LocationWrapper = styled.div `
+grid-area: location;
+display: flex;
+flex-wrap: nowrap;
+`
+
+const LikeWrapper = styled.div `
+grid-area: likeShare;
+`
+const HeaderWrapper = styled.div `
+width: 1280px;
+display: grid;
+overflow: visible!important!;
+
+grid-gap:8px;
+grid-template-columns: 80px 260px auto auto 150px;
+grid-template-rows: 60px 60px;
+  grid-template-areas:  "title title title . ."
+                        "rating location . . likeShare";
+                        align-items: center;
+`
+
+function Header({data}) {
+const location = data[0].location;
+const city = location.split(',')[0];
+const name = data[0].name;
+const isLiked = data[0].is_liked
+
   return (
 
     <div>
+      <HeaderWrapper >
 
-      <section>
-        <div >
-          <div ><h1 tabIndex="-1">Spacious two-bedroom in San Francisco Victorian</h1></div>
-          <div >
-            <span >
-              <span ><span  aria-hidden="true">󰀄</span></span>
-              <span >
 
-              </span>
-            </span>
+          <Title city={city} name={name} >
+          <div className ="title"><h1 tabIndex="-1">{name} in {city} </h1></div>
+          </Title>
 
-            <span aria-hidden="true">·</span>
-            <span ><span ><a rel="noopener noreferrer"  href="https://airbnb.com/s/San%20Francisco,California,United%20States/homes" >San Francisco, California, United States</a></span></span>
-          </div>
-          <LikeShare />
-        </div>
-      </section>
+            <RatingWrapper>
+              <Rating data={data}/>
+            </RatingWrapper>
+            <LocationWrapper data={location}>
+            <span ><span ><a rel="noopener noreferrer"  href="https://airbnb.com/s/San%20Francisco,California,United%20States/homes" >{location}</a></span></span>
+            </LocationWrapper>
+          <LikeWrapper>
+          <LikeShare isLiked={isLiked} />
+          </LikeWrapper>
+      </HeaderWrapper>
     </div>
 
   );
--- seed.js
+++ seed.js
@@ -17,10 +17,37 @@ const address = () => {
   return `${cities[faker.random.number(8)]}, ${state[faker.random.number(2)]}, United States`;
 };
 
+// Random secquental number array generator and shuffle.
+
+const photoSetGen = function () {
+  const orderArray = [];
+  let max = faker.random.number({ min: 7, max: 15 });
+
+  for (let i = 0; i <= max; i += 1) {
+    orderArray.push(i);
+  }
+  // shuffle array
+  const shuffledArr = [];
+  const copy = orderArray.slice();
+
+  const min = 0;
+console.log(copy,"copy")
+
+  copy.forEach((number, index) => {
+    const randomNum = Math.floor((Math.random() * (max - min + 1)));
+    var current = copy[index]
+    var ranInt = copy[randomNum]
+    copy[index] = ranInt;
+    copy[randomNum] = current;
+
+  });
+  return JSON.stringify(copy);
+};
+
 // seeding function takes in "seed" as an argument for the number of amount of records created.
 const seeder = (seed) => {
   for (let i = 0; i < seed; i += 1) {
-    const param = [null, 'Bobs place', faker.random.number({ min: 1, max: 5 }), faker.random.number(1), faker.random.number(2), address(), faker.random.number({ min: 1, max: 6 }), faker.random.number({ min: 5, max: 10 })];
+    const param = [null, 'Bobs place', faker.random.number({ min: 1, max: 5 }), faker.random.number(1), faker.random.number(2), address(), faker.random.number({ min: 1, max: 6 }), photoSetGen()];
     console.log(param, typeof faker.random.number());
 
     connection.query('INSERT INTO gallery VALUES (?,?,?,?,?,?,?,?)', param, (err, sucsess) => {
@@ -34,3 +61,5 @@ const seeder = (seed) => {
 };
 
 seeder(100);
+
+console.log(photoSetGen())
