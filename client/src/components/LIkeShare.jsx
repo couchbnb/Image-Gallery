6ae08bbeda4e210408191cd22f6af0ea0e0df611
Ryan De Coster <ryandecoster@gmail.com>
2020-10-03 01:47:38 -0700

    Heart renders on condtions. But my heart dosnt

--- client/src/components/LIkeShare.jsx
+++ client/src/components/LIkeShare.jsx
@@ -22,14 +22,15 @@ const LikeFrame = styled.div`
 
  svg{
 
-   fill ${(thing) =>  thing? "rgb(255, 56, 92)" : "rgb(53,50,204)"};
+   fill :${(props) =>  props.isLiked? "rgb(255, 56, 92)" : "rgb(53,50,204)"};
  }
 `;
 
 
 function LikeShare(props) {
 // const likeShare = props.likeShare;
-const likeShare = false
+const isLiked = props.isLiked
+console.log(isLiked, "LikeShare")
 
   return (
     <div>
@@ -45,7 +46,7 @@ const likeShare = false
 
 
         <Button>
-          <LikeFrame thing={false}>
+          <LikeFrame isLiked={isLiked}>
           <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" display="block"  style={{ height: '16px', width: '16px' }} stroke="currentcolor" strokeWidth="2" overflow="visible">
             <path d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z" />
             </svg>
