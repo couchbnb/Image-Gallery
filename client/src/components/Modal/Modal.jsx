a61f890cf1dbb5cc0c647f9724375fd3993eda10
Ryan De Coster <ryandecoster@gmail.com>
2020-10-04 22:46:04 -0700

    updated git ignore

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -2,41 +2,34 @@ import React from 'react';
 
 import styles from '../../css/model.module.css'
 
-
 import ReactDOM from 'react-dom';
 
-// This containers is a sibling to the 'root' element in the DOM
 const modalRoot = document.getElementById('modal');
 
-// Let's create a Modal component that is an abstraction around the portal API.
 class Modal extends React.Component {
   constructor(props) {
     super(props);
-    // Create a div that we'll render the modal into.
+
     this.el = document.createElement('div');
   }
 
   componentDidMount() {
-    // Append the element into the DOM on mount. We'll render
-    // into the modal container element (see the HTML tab).
     modalRoot.appendChild(this.el);
   }
 
   componentWillUnmount() {
-    // Remove the element from the DOM when we unmount
     modalRoot.removeChild(this.el);
   }
 
   render() {
-    // Use a portal to render the children into the element
+    let{children}=this.props;
     return ReactDOM.createPortal(
-      // Any valid React child: JSX, strings, arrays, etc.
-      this.props.children,
-      // A DOM element
+
+      children,
+
       this.el,
     );
   }
 }
 
 export default Modal;
-// initial modal set up
