ae9a2844eccf1c4525346e98e98e9dd89456d565
Ryan De Coster <50038256+Rdecoster@users.noreply.github.com>
2020-10-09 22:08:08 -0700

    Add files via upload

---
+++ database/schema.sql
@@ -0,0 +1,22 @@
+
+    -- mysql -u root -p < schema.sql
+
+DROP DATABASE IF EXISTS couchbnb;
+
+CREATE DATABASE couchbnb;
+
+USE couchbnb;
+
+CREATE TABLE gallery (
+id int not null AUTO_INCREMENT,
+name VARCHAR(50),
+rating DECIMAL(5,2),
+reviews INT,
+is_super INT,
+is_liked INT,
+location VARCHAR(100),
+photo_set INT,
+number_photo TEXT,
+PRIMARY KEY(id)
+);
+
