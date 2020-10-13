b734a731ea7e8deac53d7289a426eaa12bd3b0c4
Ryan De Coster <ryandecoster@gmail.com>
2020-10-13 00:52:28 -0700

    fixed allignment issue with styling.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,19 @@ import styled from 'styled-components';
 import LikeShare from './LikeShare.jsx';
 import Rating from './Rating.jsx';
 
+const RatingWrapper = styled.div`
+
+display: flex;
+align-items: flex-start;
+grid-area: rating;
+color: #717171;
+width: 300px;
+`;
+
 const Title = styled.div`
 grid-area: title;
 display:flex;
+flex-direction: row;
 font-size: 26px !important!;
 line-height:30px!important!;
 text-align:start!important!;
@@ -13,63 +23,105 @@ font-weight: normal !important!;
 padding: 0 !important!;
 `;
 
-const RatingWrapper = styled.div`
-grid-area: rating;
-color: #717171;
+const Star = styled.div`
+grid-area: star;
 
-span{
-  margin: 4px ;
-  a, a:visited {
-    color:  #717171;
-  };
+      cursor: pointer;
+      position: relative;
+      overflow: visible;
+      stroke: #ff385c;
+      fill: #ff385c;
+      align: center;
+      font-size: 14px !important;
+      padding-bottom: 5px;
+      /* line-height:15px !important; */
+
+    img{
+      position: sticky;
+      height: 15px;
+      width: 15px;
+      top: 50%;
+      left: 40rh;
+        }
 
 `;
 
+const Location = styled.div`
+
+cursor: pointer;
+position: auto;
+grid-area: location;
+text-decoration: underline;
+top:100%;
+`;
 const LikeWrapper = styled.div`
+display: flex;
+align-items: center;
 grid-area: likeShare;
+
 `;
 const HeaderWrapper = styled.div`
+position: relative;
 width: 1280px;
-display: grid;
+display: inline-grid;
 overflow: visible!important!;
-flex-wrap: nowrap !important!;
+flex-wrap: nowrap;
+align-items: center;
 
-margin:30px !important;
+margin:16px;
 
 grid-gap:0px;
-grid-template-columns: auto auto auto auto auto 20px;
-grid-template-rows: 50px 40px;
-  grid-template-areas:  "title title title . . ."
-                        "rating . . . .likeShare";
-                        align-items: center;
+grid-template-columns: 100px 100px 250px auto auto auto 200px;
+grid-template-rows:  50px 25px;
+  grid-template-areas:   " title title title title . . . "
+                        " star rating  location location . . likeShare ";
+
 `;
 
-function Header({ data, updateLike, isSuperHost}) {
+function Header({ data, updateLike, isSuperHost }) {
   const { location } = data[0];
   const city = location.split(',')[0];
   const { name } = data[0];
-  const isLiked = isSuperHost
-
-
+  const isLiked = isSuperHost;
+  const { reviews } = data[0];
+  const { rating } = data[0];
   return (
 
     <div>
       <HeaderWrapper>
-
         <Title city={city} name={name}>
           <div className="title">
             <h1 tabIndex="-1">
-              {name} in {city}
+              {name}
+              {' '}
+              in
+              {city}
             </h1>
           </div>
         </Title>
 
+        <Star data={reviews}>
+          <div>
+
+            <img src="star.png" />
+            <b>{rating}</b>
+            {' '}
+
+            {(`(${reviews || ''})`)}
+
+            <div>{}</div>
+
+          </div>
+        </Star>
+
         <RatingWrapper data={location}>
-          <Rating data={data}/>
-          <span><a href="https://airbnb.com/s/San%20Francisco,California,United%20States/homes"><b>{location}</b></a></span>
+          <Rating data={data} />
         </RatingWrapper>
+        <Location>
+          {location}
+        </Location>
         <LikeWrapper>
-          <LikeShare isLiked={isLiked} updateLike ={updateLike} />
+          <LikeShare isLiked={isLiked} updateLike={updateLike} />
         </LikeWrapper>
       </HeaderWrapper>
     </div>
