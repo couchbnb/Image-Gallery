c69ed3b118e391c7a0200db42a74ecd49297106b
Ryan De Coster <50038256+Rdecoster@users.noreply.github.com>
2020-10-13 11:40:35 -0700

    Update index.js
    Changes for deployment

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  // host: 'mysql',
+  //host: 'localhost',
+   host: 'mysql',
   user: 'root',
-  // password: 'student',
+  password: 'student',
 
   database: 'couchbnb',
 
